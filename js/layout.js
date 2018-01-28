64242ae23ebad825dbefffde1e83cac340e6f9f2
zcramar87 <35750189+zcramar87@users.noreply.github.com>
2018-01-27 20:23:29 -0500

    Update layout.js

--- js/layout.js
+++ js/layout.js
@@ -27,7 +27,7 @@ var Layout = function () {
             $('.navbar-fixed-top').addClass('top-nav-collapse');
         }
         $(window).scroll(function() {
-            if ($('.navbar').offset().top > 150) {
+            if ($('.navbar').length && $('.navbar').offset().top > 150) {
                 $('.navbar-fixed-top').addClass('top-nav-collapse');
             } else {
                 $('.navbar-fixed-top').removeClass('top-nav-collapse');
